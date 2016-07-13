d5a52e8242c8451c5f70b6c79fb02ea189667492
Vladimir Gaydamaka <gaydamaka.vov@gmail.com>
2016-07-13 12:58:36 +0300

    Impoved after review

--- commerce_adyen_pro.api.php
+++ commerce_adyen_pro.api.php
@@ -1,21 +1,21 @@
 <?php
+
 /**
  * @file
- * Hook documentation for the Adyen PRO module.
+ * Hook documentation for the PayPal WPS module.
  */
-
 
 /**
  * Allows modules to alter the data array used to create a Adyen redirect
  * form prior to its form elements being created.
  *
- * @param &$data
+ * @param array &$data
  *   The data array used to create redirect form elements.
- * @param $order
+ * @param \stdClass $order
  *   The full order object the redirect form is being generated for.
  *
  * @see commerce_adyen_pro_redirect_form()
  */
-function hook_commerce_adyen_pro_redirect_form_data_alter(&$data, $order) {
+function hook_commerce_adyen_pro_redirect_form_data_alter(array &$data, \stdClass $order) {
   // No example.
 }
