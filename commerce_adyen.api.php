8536015662630007d6dd2cbcc19d6693f158d98c
gajdamaka <gaydamaka.vov@gmail.com>
2016-07-20 23:26:01 +0300

    LGR-1161: Commerce Adyen (#1680)
    * Add custom module 'Lush Adyen' in profiles dependencies
    
    * Add test account for Adyen
    
    * Create custom implementation for Adyen
    
    * [skip ci] Change comment
    
    * Add 'Capture request' implementation
    
    * Move capture request in custom rule action and define notification handling for each notification type
    
    * Add return operator
    
    * [skip ci] Add comment to function
    
    * Use 'element_children()'
    
    * Change option name
    
    * Replace "commerce_adyen_pro" by "commerce_adyen" module
    
    * Add PHPDoc comment for the "_lush_payment_rule_settings()" function
    
    * Add integration with "commerce_adyen" by "lush_adyen"
    
    * Replace wrong "hook_update" by "hook_install" because "lush_adyen" has not been already installed
    
    * LGR-1161 fix two typos when preparing payment request and fix clearing drupal form elements in pre-render hook (we can not use iterator for array_keys() function).
    
    * LGR-1161 change transactions flow.
    
    * LGR-1161 add a rule for OS side to capture transaction from Adyen when it is dispatched.
    
    * LGR-1161 [skip-ci] add todo comment.
    
    * LGR-1161 fix a bit namespaces.
    
    * LGR-1161 use libraries method to call capture request.
    
    * LGR-1161 use Adyen library instead of curl.
    
    * LGR-1161 remove not-needed dependency.
    
    * Rename "commerce_adyen_hmac_signature()" by "commerce_adyen_sha_signature()", use Adyen util for generating SHA signatures, improve "commerce_adyen_capture_request()".
    
    * Add "composer_manager" as a dependency to "commerce_adyen"
    
    * Use constants from Adyen lib for application mode selection, improve form clearing, add documentation
    
    * Add "composer.json" to "commerce_adyen" with "adyen/php-api-library" as requirement
    
    * LGR-1161 fix payment request.
    
    * LGR-1161 return payment transaction in capture request to allow other modules to know state of capture request.
    
    * LGR-1161 add payment fails message to be used by others modules and removed not needed rule (handled by another way).
    
    * LGR-1161 fix phpcs notes.
    
    * Improve the logic of "commerce_adyen_capture_request()", fix PHPDoc
    
    * Remove support of deprecated "SHA1" signature
    
    * Removed "!empty($order)" condition since "$order" is of "\stdClass" type
    
    * Removed the "getKeys()" method and use "iterator_to_array()" instead
    
    * Removed "algorithm" setting from "lush_adyen", use constants from Adyen PHP lib

---
+++ commerce_adyen.api.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @file
+ * Commerce Adyen API.
+ */
+
+/**
+ * Register Adyen response types.
+ *
+ * @see commerce_adyen_responses()
+ *
+ * @return \Commerce\Adyen\ResponseInterface[]
+ *   A list of response types.
+ */
+function hook_commerce_adyen_responses() {
+  return [
+    \Commerce\Adyen\Response\Payment::class,
+    \Commerce\Adyen\Response\Notification::class,
+  ];
+}
+
+/**
+ * Allow alter the data used to create redirect form to Adyen.
+ *
+ * @param \Commerce\Adyen\Request\Payment $payment
+ *   The data used to create redirect form.
+ * @param \stdClass $order
+ *   The full order object the redirect form is being generated for.
+ *
+ * @see commerce_adyen_redirect_form()
+ */
+function hook_commerce_adyen_payment_request_alter(\Commerce\Adyen\Request\Payment $payment, \stdClass $order) {
+  $payment->setSessionValidity(strtotime('+ 2 hour'));
+  $payment->setShopperLocale(user_load($order->uid)->language);
+}
+
+/**
+ * React on a notification from Adyen.
+ *
+ * @link https://docs.adyen.com/developers/api-manual#notificationfields
+ *
+ * @see \Commerce\Adyen\Response\Notification::__construct()
+ *
+ * @param string $event_code
+ *   One of event codes in a lowercase.
+ * @param array $data
+ *   Submitted data.
+ */
+function hook_commerce_adyen_notification($event_code, array $data) {
+  if ('cancellation' === $event_code) {
+    $order = commerce_order_load_by_number($data['merchantReference']);
+    $transactions = commerce_payment_transaction_load_multiple([], [
+      'order_id' => $order->order_id,
+      'instance_id' => COMMERCE_ADYEN_PAYMENT_METHOD_INSTANCE,
+      'payment_method' => COMMERCE_ADYEN_PAYMENT_METHOD,
+    ]);
+
+    $transaction = reset($transactions);
+    $transaction->status = COMMERCE_PAYMENT_STATUS_FAILURE;
+    commerce_payment_transaction_save($transaction);
+  }
+}
