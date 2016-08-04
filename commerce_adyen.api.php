1e075f255fc7404f4c21fa61b7b13bf65c1319c9
Sergii Bondarenko <sb@firstvector.org>
2016-08-04 17:12:30 +0300

    LGR-1732: Commerce Adyen (#1818)
    * Apply experimental approach for Adyen notifications, remove obsolete "todo"
    
    * Removed experimental "hook_commerce_adyen_notification()"
    
    * Redesign "commerce_adyen" module
    
    * Add "uid" property for Drupal transation object
    
    * LGR-1741: Show message for 'refused' or 'error' and 'cancelled' authorisation status
    
    * Added comments for "Payment/Authorization/Response"
    
    * Rectify cancelation message
    
    * Apply "commerce_adyen" API in "lush_adyen"
    
    * LGR-1749: Multiply the total by 100 if currency is HUF
    
    * Add notification handling, add "authorized" payment status
    
    * Remove "todo" from "commerce_adyen_capture_request()"
    
    * Use "authorised" instead "authorized"
    
    * Handle "capture" notifications, add "finalize" method to "Transaction" class
    
    * Replace exception by "watchdog" for transaction saving
    
    * Use "+ 2 hours" as "sessionValidity"

--- commerce_adyen.api.php
+++ commerce_adyen.api.php
@@ -5,33 +5,44 @@
  */
 
 /**
- * Register Adyen response types.
+ * Allow alter the data used to create payment authorization.
  *
- * @see commerce_adyen_responses()
- *
- * @return \Commerce\Adyen\ResponseInterface[]
- *   A list of response types.
- */
-function hook_commerce_adyen_responses() {
-  return [
-    \Commerce\Adyen\Response\Payment::class,
-    \Commerce\Adyen\Response\Notification::class,
-  ];
-}
-
-/**
- * Allow alter the data used to create redirect form to Adyen.
- *
- * @param \Commerce\Adyen\Request\Payment $payment
- *   The data used to create redirect form.
+ * @param \Commerce\Adyen\Payment\Authorization\Request $payment
+ *   Payment authorization request that will be sent to Adyen.
  * @param \stdClass $order
- *   The full order object the redirect form is being generated for.
+ *   Commerce order.
+ * @param array $payment_method
+ *   Commerce payment method.
  *
  * @see commerce_adyen_redirect_form()
  */
-function hook_commerce_adyen_payment_request_alter(\Commerce\Adyen\Request\Payment $payment, \stdClass $order) {
+function hook_commerce_adyen_payment_authorization_request_alter(\Commerce\Adyen\Payment\Authorization\Request $payment, \stdClass $order, array $payment_method) {
   $payment->setSessionValidity(strtotime('+ 2 hour'));
   $payment->setShopperLocale(user_load($order->uid)->language);
+}
+
+/**
+ * Allow alter the data used to process payment authorization.
+ *
+ * @param \Commerce\Adyen\Payment\Authorization\Response $payment
+ *   Payment authorization response that has been received from Adyen.
+ * @param \stdClass $order
+ *   Commerce order.
+ * @param array $payment_method
+ *   Commerce payment method.
+ *
+ * @see commerce_adyen_redirect_form_validate()
+ */
+function hook_commerce_adyen_payment_authorization_response_alter(\Commerce\Adyen\Payment\Authorization\Response $payment, \stdClass $order, array $payment_method) {
+  switch ($payment->getAuthenticationResult()) {
+    case $payment::ERROR:
+      $transaction = $payment->getTransaction();
+      $transaction->setStatus(COMMERCE_PAYMENT_STATUS_FAILURE);
+      $transaction->setMessage('Payment cannot be completed. Adyen response: @response.', [
+        '@response' => $payment->getReceivedData(),
+      ]);
+      break;
+  }
 }
 
 /**
@@ -39,24 +50,23 @@ function hook_commerce_adyen_payment_request_alter(\Commerce\Adyen\Request\Payme
  *
  * @link https://docs.adyen.com/developers/api-manual#notificationfields
  *
- * @see \Commerce\Adyen\Response\Notification::__construct()
- *
  * @param string $event_code
  *   One of event codes in a lowercase.
- * @param array $data
- *   Submitted data.
+ * @param \stdClass $order
+ *   Commerce order.
+ * @param \stdClass $data
+ *   Received data (from $_REQEUST superglobal).
+ *
+ * @see commerce_adyen_notification()
  */
-function hook_commerce_adyen_notification($event_code, array $data) {
-  if ('cancellation' === $event_code) {
-    $order = commerce_order_load_by_number($data['merchantReference']);
-    $transactions = commerce_payment_transaction_load_multiple([], [
-      'order_id' => $order->order_id,
-      'instance_id' => COMMERCE_ADYEN_PAYMENT_METHOD_INSTANCE,
-      'payment_method' => COMMERCE_ADYEN_PAYMENT_METHOD,
-    ]);
+function hook_commerce_adyen_notification($event_code, \stdClass $order, \stdClass $data) {
+  switch ($event_code) {
+    case \Commerce\Adyen\Payment\NotificationInterface::CANCELLATION:
+      $transaction = new \Commerce\Adyen\Payment\Transaction($order);
+      $transaction->setStatus(COMMERCE_PAYMENT_STATUS_FAILURE);
+      $transaction->save();
 
-    $transaction = reset($transactions);
-    $transaction->status = COMMERCE_PAYMENT_STATUS_FAILURE;
-    commerce_payment_transaction_save($transaction);
+      commerce_order_status_update($order, 'canceled');
+      break;
   }
 }
