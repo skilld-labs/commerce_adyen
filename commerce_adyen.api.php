f19e2be768b92fa5d75b62dc8dc70b2c94bf7892
Sergii Bondarenko <sb@firstvector.org>
2016-08-26 11:00:45 +0300

    LGR-1670: OpenInvoice payments (Klarna) (#1941)

--- commerce_adyen.api.php
+++ commerce_adyen.api.php
@@ -155,3 +155,76 @@ function hook_commerce_adyen_payment_types() {
 function hook_commerce_adyen_payment_types_alter(array &$payment_types) {
   unset($payment_types['openinvoice']);
 }
+
+/**
+ * Change default values of the address.
+ *
+ * @param \Commerce\Adyen\Payment\Composition\Address $address
+ *   Address for OpenInvoice Adyen payment.
+ * @param \EntityDrupalWrapper $order
+ *   Metadata wrapper for "commerce_order" entity.
+ * @param \EntityDrupalWrapper $profile
+ *   Commerce customer profile.
+ *
+ * @see \Commerce\Adyen\Payment\OpenInvoice\PaymentController::addAddress()
+ */
+function hook_commerce_adyen_shopper_address_alter(\Commerce\Adyen\Payment\Composition\Address $address, \EntityDrupalWrapper $order, \EntityDrupalWrapper $profile) {
+  if ('Dnipropetrovsk' === $address->getCity()) {
+    $address->setCity('Dnipro');
+  }
+}
+
+/**
+ * Change default values of shopper information.
+ *
+ * @param \Commerce\Adyen\Payment\Composition\Shopper $shopper
+ *   Shopper information for OpenInvoice Adyen payment.
+ * @param \EntityDrupalWrapper $order
+ *   Metadata wrapper for "commerce_order" entity.
+ * @param \EntityDrupalWrapper $billing
+ *   Commerce customer profile.
+ *
+ * @see \Commerce\Adyen\Payment\OpenInvoice\PaymentController::addShopperInformation()
+ */
+function hook_commerce_adyen_shopper_information_alter(\Commerce\Adyen\Payment\Composition\Shopper $shopper, \EntityDrupalWrapper $order, \EntityDrupalWrapper $billing) {
+  $shopper->setFirstName('Sergii');
+}
+
+/**
+ * React on initialization of checkout fields.
+ *
+ * @param array[] $instances
+ *   Drupal field definitions.
+ * @param \EntityDrupalWrapper $order
+ *   Wrapper for the "commerce_order" entity.
+ * @param \Commerce\Adyen\Payment\Controller\Checkout $controller
+ *   An instance of checkout controller.
+ * @param string $type
+ *   Machine name of payment type.
+ *
+ * @see commerce_adyen_submit_form()
+ */
+function hook_commerce_adyen_checkout_fields_init(array $instances, \EntityDrupalWrapper $order, \Commerce\Adyen\Payment\Controller\Checkout $controller, $type) {
+
+}
+
+/**
+ * React on saving the values of checkout fields.
+ *
+ * This hook will be triggered only if validation of the controller
+ * will be succeed.
+ *
+ * @param array $values
+ *   Values of checkout form.
+ * @param \EntityDrupalWrapper $order
+ *   Wrapper for the "commerce_order" entity.
+ * @param \Commerce\Adyen\Payment\Controller\Checkout $controller
+ *   An instance of checkout controller.
+ * @param string $type
+ *   Machine name of payment type.
+ *
+ * @see commerce_adyen_submit_form_submit()
+ */
+function hook_commerce_adyen_checkout_fields_save(array $values, \EntityDrupalWrapper $order, \Commerce\Adyen\Payment\Controller\Checkout $controller, $type) {
+
+}
